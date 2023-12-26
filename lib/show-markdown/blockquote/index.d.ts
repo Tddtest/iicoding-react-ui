2602c3fbe166a5c7f9aec1a2b7d5273f2b026873
iiCoding <297854770@qq.com>
2023-12-26 17:50:36 +0800

    部分修改

--- lib/show-markdown/blockquote/index.d.ts
+++ lib/show-markdown/blockquote/index.d.ts
@@ -1,3 +1,11 @@
+/**
+ * @auth: dmx
+ * @time: 2022/6/16
+ * @func:
+ * @params:
+ * @return:
+ * @updateTime:
+ **/
 import * as React from 'react';
 import './index.less';
 interface IProps extends ITagProps {
